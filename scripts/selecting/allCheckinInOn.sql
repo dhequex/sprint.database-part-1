4100101389657b0670ead9057bdc6c15df9162e0
Jose M. Torres <jmtorresr@gmail.com>
2020-10-08 07:15:19 +0900

    Updated queries on joins section

--- scripts/selecting/allCheckinInOn.sql
+++ scripts/selecting/allCheckinInOn.sql
@@ -1,4 +1,6 @@
 
-SELECT * FROM students
-INNER JOIN checkins
-ON students.id = checkins.id;
+SELECT students.id,students.first_name,checkins.checked_in_at
+FROM STUDENTS
+INNER JOIN checkins 
+ON students.id = checkins.id
+WHERE checked_in_at  between '2016-06-01 00:00:00' and '2016-06-30 24:00:00';
--- scripts/selecting/studentsOnProjectX.sql
+++ scripts/selecting/studentsOnProjectX.sql
@@ -1,1 +1,5 @@
--- Your SQL here
+SELECT students.first_name, students_last_name, students_to_projects/project_id
+FROM students
+INNER JOIN students_to_projects
+ON students.id = students_to_projects.student_id
+WHERE students_to_projects.project_id =5;
